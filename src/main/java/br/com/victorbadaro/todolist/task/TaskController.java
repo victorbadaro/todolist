9dd4ffa1288a92efcb3acffaa6df7b704541fa37
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-10-12 17:04:37 -0300

    feat: add methods to update a specific task

--- src/main/java/br/com/victorbadaro/todolist/task/TaskController.java
+++ src/main/java/br/com/victorbadaro/todolist/task/TaskController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +52,15 @@ public class TaskController {
     var tasks = this.taskRepository.findByIdUser((UUID) idUser);
 
     return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    var idUser = request.getAttribute("idUser");
+
+    taskModel.setId(id);
+    taskModel.setIdUser((UUID) idUser);
+
+    return this.taskRepository.save(taskModel);
   }
 }
