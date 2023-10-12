2f6d2a4002ea3c27997a6d1b49aab326945405bc
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-10-12 16:41:10 -0300

    chore: improve rules for creating tasks

--- src/main/java/br/com/victorbadaro/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/victorbadaro/todolist/filter/FilterTaskAuth.java
@@ -23,28 +23,35 @@ public class FilterTaskAuth extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    var authorization = request.getHeader("Authorization");
-    var authEncoded = authorization.substring("Basic".length()).trim();
+    var servletPath = request.getServletPath();
 
-    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+    if (servletPath.equals("/tasks/")) {
+      var authorization = request.getHeader("Authorization");
+      var authEncoded = authorization.substring("Basic".length()).trim();
 
-    var authString = new String(authDecode);
-    String[] credentials = authString.split(":");
-    String username = credentials[0];
-    String password = credentials[1];
+      byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-    var user = this.userRepository.findByUsername(username);
+      var authString = new String(authDecode);
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
 
-    if (user == null) {
-      response.sendError(401);
-    } else {
-      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+      var user = this.userRepository.findByUsername(username);
 
-      if (passwordVerify.verified) {
-        filterChain.doFilter(request, response);
-      } else {
+      if (user == null) {
         response.sendError(401);
+      } else {
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+        if (passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401);
+        }
       }
+    } else {
+      filterChain.doFilter(request, response);
     }
   }
 }
